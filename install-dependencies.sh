460fe52b42100c9cdd5e719d1dd39d81e1be01de
ibrahim badr <ibrahimbdr@outlook.com>
2023-05-11 17:53:11 +0300

    adding script to run all apps

---
+++ install-dependencies.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Clean npm cache in each project folder
+echo "Cleaning npm cache..."
+cd server && sudo npm cache clean --force
+cd ../dashboard && sudo npm cache clean --force
+cd ../client && sudo npm cache clean --force
+
+# Check npm version and update if necessary
+echo "Checking npm version..."
+npm_version=$(npm --version)
+latest_npm_version=$(npm view npm version)
+
+if [[ $npm_version != $latest_npm_version ]]; then
+  echo "Updating npm to the latest version..."
+  npm install -g npm@latest
+fi
+
+# Delete node_modules in each project folder
+echo "Deleting node_modules..."
+rm -rf server/node_modules
+rm -rf dashboard/node_modules
+rm -rf client/node_modules
+
+# Delete package-lock.json
+# echo "Deleting package-lock.json..."
+# rm -f server/package-lock.json
+# rm -f dashboard/package-lock.json
+# rm -f client/package-lock.json
+
+# Create .env file in the client folder
+# Create .env file in the client folder if it doesn't exist
+echo "Creating .env file..."
+if [[ -f client/.env ]]; then
+  read -p "A .env file already exists. Do you want to overwrite it? (y/n): " overwrite
+  if [[ $overwrite != "y" && $overwrite != "Y" ]]; then
+    echo "Skipping .env file creation..."
+  else
+    echo "PORT=3001" > client/.env
+    echo "Successfully created a new .env file."
+  fi
+else
+  echo "PORT=3001" > client/.env
+  echo "Successfully created a new .env file."
+fi
+
+# Create .env file in the server folder
+echo "Creating .env file in the server folder..."
+echo "PORT=4040" > server/.env
+echo "DATABASE_URL=mongodb://127.0.0.1:27017/agenda_app" >> server/.env
+echo "JWT_SECRET=v*N\"fpu9IFJHh1FqgK~''@YS55&u5J" >> server/.env
+echo "Successfully created a new .env file in the server folder."
+
+# Install dependencies using npm install
+echo "Installing dependencies with npm install..."
+cd server && npm install
+cd ../dashboard && npm install
+cd ../client && npm install
+
+# Installing Concurrently as a dev-dependency
+echo "Installing concurrently"
+cd ../ && npm install --save-dev concurrently
+
+echo "Dependencies installation completed successfully!"
+
+# Running the App
+echo "Running agenda-app"
+npm run all
+
