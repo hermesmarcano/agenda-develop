1a8ed2761fc86b6f9a5ce2b34f63c804ee8d4629
ibrahim badr <ibrahimbdr@outlook.com>
2023-08-25 05:39:31 +0300

    update

--- client/src/pages/BookingCompleted/bookingCompleted.js
+++ client/src/pages/BookingCompleted/bookingCompleted.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import instance from "../axiosConfig/axiosConfig";
+import instance from "../../axiosConfig/axiosConfig";
 
 function BookingCompleted() {
   const navigate = useNavigate();
